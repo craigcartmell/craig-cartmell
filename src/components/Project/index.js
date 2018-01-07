74a7259ef86d92c1638d5016009d7295c06d5d2e
Craig Cartmell <craigcartmell1@gmail.com>
2018-01-07 17:18:18 +0000

    Icons and button styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import ReactIcon from '../DevIcons/React'
-import PHPIcon from '../DevIcons/PHP'
-import MySQL from "../DevIcons/MySql"
-import Sass from "../DevIcons/Sass"
-import Babel from "../DevIcons/Babel"
-import Laravel from "../DevIcons/Laravel"
-import JavaScript from "../JavaScript"
-import Node from "../DevIcons/Node"
-import Webpack from "../DevIcons/Webpack"
-import Gulp from "../DevIcons/Gulp"
-import HTML5 from "../DevIcons/HTML5"
-import Sequelize from "../DevIcons/Sequelize"
-import CodeIgniter from "../DevIcons/CodeIgniter"
-import CSS3 from "../DevIcons/CSS3"
 
 const Project = ({
     color,
     heroSrc,
-    jobTitle = "Senior Full Stack Developer",
+    jobTitle = 'Senior Full Stack Developer',
     title,
     description,
     icons,
-    role = "",
+    role = '',
     style = { clipPath: 'polygon(0 0, 100% 0, 100% 66%, 0 60%)' },
-    url,
+    url
 }) => (
     <div
         className={`w-1/4 h-full overflow-hidden shadow-lg bg-${color} text-white`}
@@ -43,34 +29,22 @@ const Project = ({
             <h3 className="h-3 mt-6 mb-6">{jobTitle}</h3>
             <p className="text-sm mt-3">{role}</p>
         </div>
-        <div className="px-4 py-2 ">
+        <div className="px-4 py-2">
             <span>
-                <ReactIcon className="w-10"/>
-                <PHPIcon className="w-10"/>
-                <MySQL className="w-10"/>
-                <Sass className="w-10"/>
-                <Babel className="w-10"/>
-                <Laravel className="w-10"/>
-                <JavaScript className="w-10"/>
-                <Node className="w-10"/>
-                <Webpack className="w-10"/>
-                <Gulp className="w-10"/>
-                <HTML5 className="w-10"/>
-                <Sequelize className="w-10"/>
-                <CodeIgniter className="w-10"/>
-                <CSS3 className="w-10"/>
-                {/*{icons.map(icon => (*/}
-                    {/*<Icon icon={icon} size={32} className="mr-1" />*/}
-                {/*))}*/}
+                {icons &&
+                    icons.map(icon =>
+                        React.cloneElement(icon, { className: 'w-8 mr-2' })
+                    )}
             </span>
-          {url ? <span className="float-right">
-              <a href={url} rel="noopener noreferrer" target="_blank">
-                <button
-                  className="bg-transparent hover:bg-pink-dark text-white hover:text-pink-light font-bold py-2 px-4 border border-white rounded">
-                  View
-                </button>
-              </a>
-            </span> : null}
+            {url ? (
+                <span className="float-right">
+                    <a href={url} rel="noopener noreferrer" target="_blank">
+                        <button className="bg-transparent hover:bg-white text-white hover:text-blue-dark font-bold py-2 px-4 border border-white rounded">
+                            View
+                        </button>
+                    </a>
+                </span>
+            ) : null}
         </div>
     </div>
 );
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import Project from "../Project"
+import Project from '../Project';
+import ReactIcon from '../DevIcons/React';
+import PHPIcon from '../DevIcons/PHP';
+import MySQL from '../DevIcons/MySql';
+import Sass from '../DevIcons/Sass';
+import Babel from '../DevIcons/Babel';
+import Laravel from '../DevIcons/Laravel';
+import JavaScript from '../JavaScript';
+import Node from '../DevIcons/Node';
+import Webpack from '../DevIcons/Webpack';
+import Gulp from '../DevIcons/Gulp';
+import HTML5 from '../DevIcons/HTML5';
+import Sequelize from '../DevIcons/Sequelize';
+import CodeIgniter from '../DevIcons/CodeIgniter';
+import CSS3 from '../DevIcons/CSS3';
 
 import exxonSalesHero from '../../images/projects/exxon-mobil-sales-supervisor-excellence.png';
 import scHero from '../../images/projects/jcdecaux-smart-content-new.png';
@@ -10,88 +24,122 @@ import scDashboardHero from '../../images/projects/jcdecaux-smart-content-dashbo
 
 const projectsData = [
     {
-        color: "pink",
-        description: "SmartContent is a single page content management platform for managing digital out of home ads.",
-        jobTitle: "Senior Full Stack Developer",
-        role: "Initially hired as a freelancer to support the recently created MVP, I moved into a permanent role to " +
-        "help rebuild the content management system from the ground up. I was responsible " +
-        "for the initial front-end platform build including liaising with the UX designer, choosing the " +
-        "tech stack and creating the initial boilerplate.",
+        color: 'pink',
+        description:
+            'SmartContent is a single page content management platform for managing digital out of home ads.',
+        icons: [
+            <ReactIcon />,
+            <JavaScript />,
+            <Laravel />,
+            <PHPIcon />,
+            <MySQL />,
+            <Sass />,
+            <Webpack />,
+            <Babel />
+        ],
         heroSrc: scHero,
-        style: {clipPath: "polygon(0px 0px, 100% 0px, 100% 100%, 0px 90%)"},
-        title: "SmartContent for Dynamic, JCDecaux",
+        jobTitle: 'Senior Full Stack Developer',
+        role:
+            'Initially hired as a freelancer to support the recently created MVP, I moved into a permanent role to ' +
+            'help rebuild the content management system from the ground up. I was responsible ' +
+            'for the initial front-end platform build including liaising with the UX designer, choosing the ' +
+            'tech stack and creating the initial boilerplate.',
+        style: { clipPath: 'polygon(0px 0px, 100% 0px, 100% 100%, 0px 90%)' },
+        title: 'SmartContent for Dynamic, JCDecaux'
     },
     {
-        color: "indigo",
-        description: "A single page, real-time scDashboardHero showing job failures and campaign analytics.",
+        color: 'indigo',
+        description:
+            'A single page, real-time scDashboardHero showing job failures and campaign analytics.',
         heroSrc: scDashboardHero,
-        jobTitle: "Senior Full Stack Developer",
-        role: "A pet project to display critical information about campaigns and system performance. I built it using Apollo 2 " +
-        "(GraphQL) server for the backend and Apollo 2 client with React for the front-end.",
-        title: "SmartContent Dashboard for Dynamic, JCDecaux",
+        icons: [
+            <ReactIcon />,
+            <JavaScript />,
+            <MySQL />,
+            <Sequelize />,
+            <Webpack />,
+            <Babel />
+        ],
+        jobTitle: 'Senior Full Stack Developer',
+        role:
+            'A pet project to display critical information about campaigns and system performance. I built it using Apollo 2 ' +
+            '(GraphQL) server for the backend and Apollo 2 client with React for the front-end.',
+        title: 'SmartContent Dashboard for Dynamic, JCDecaux',
         style: {
             clipPath: 'polygon(0 0, 100% 0, 100% 76%, 0 86%)'
         },
-        url: "https://github.com/craigcartmell/scDashboardHero"
+        url: 'https://github.com/craigcartmell/scDashboardHero'
     },
     {
-        color: "teal",
-        description: "The tool helps employees measure their own performance as well as allowing managers and" +
-        " directors to view the statistical data using charts and graphs.",
+        color: 'teal',
+        description:
+            'The tool helps employees measure their own performance as well as allowing managers and' +
+            ' directors to view the statistical data using charts and graphs.',
         heroSrc: exxonSalesHero,
-        jobTitle: "Freelance Front-End Developer",
-        title: "ExxonMobil Sales Supervisor Excellence for KHWS",
-        role: "As the sole frontend developer, my role was to create a new Self Assessment Platform for ExxonMobil" +
-        " Sales Employees.",
+        icons: [<JavaScript />, <CSS3 />, <Gulp />],
+        jobTitle: 'Freelance Front-End Developer',
+        title: 'ExxonMobil Sales Supervisor Excellence for KHWS',
+        role:
+            'As the sole frontend developer, my role was to create a new Self Assessment Platform for ExxonMobil' +
+            ' Sales Employees.',
         style: {
             clipPath: 'polygon(0px 0px, 100% 0px, 100% 100%, 0px 94%)'
         }
     },
     {
-        color: "orange",
-        description: "A single page app which allowed Saatchi & Saatchi to find the brightest new grads. " +
-        "The winners were offered 1 year paid work placement.",
+        color: 'orange',
+        description:
+            'A single page app which allowed Saatchi & Saatchi to find the brightest new grads. ' +
+            'The winners were offered 1 year paid work placement.',
         heroSrc: theDoorHero,
-        jobTitle: "Backend Developer",
-        role: "Responsible for the full backend development, my role was to create a RESTful API to validate and " +
-        "store submitted user data. Submissions could be viewed via an admin interface which allowed searching " +
-        "and exporting of data.",
-        style: {clipPath: 'polygon(0px 0px, 100% 0px, 100% 84%, 0px 96%)'},
-        title: "The Door for Saatchi & Saatchi",
-        url: "http://thedoor.saatchi.co.uk/",
+        icons: [<PHPIcon />, <CodeIgniter />, <MySQL />],
+        jobTitle: 'Backend Developer',
+        role:
+            'Responsible for the full backend development, my role was to create a RESTful API to validate and ' +
+            'store submitted user data. Submissions could be viewed via an admin interface which allowed searching ' +
+            'and exporting of data.',
+        style: { clipPath: 'polygon(0px 0px, 100% 0px, 100% 84%, 0px 96%)' },
+        title: 'The Door for Saatchi & Saatchi',
+        url: 'http://thedoor.saatchi.co.uk/'
     },
     {
-        color: "blue",
-        description: "A single page app that allows users to create their own cartoon avatar with a " +
-        "custom slogan, and upload it to social media.",
+        color: 'blue',
+        description:
+            'A single page app that allows users to create their own cartoon avatar with a ' +
+            'custom slogan, and upload it to social media.',
         heroSrc: cravendaleMilkposterHero,
-        jobTitle: "Backend Developer",
-        role: "Responsible for the full backend development, my role was to create an API to store " +
-        "submitted user data and generate an avatar using Imagick.",
-        title: "Cravendale Milk Poster for Saatchi & Saatchi",
-        style: {clipPath: 'polygon(0px 0px, 100% 0px, 100% 99%, 0px 90%)'},
-        url: "http://www.cravendale.co.uk/milkposter/"
+        icons: [<PHPIcon />, <CodeIgniter />, <MySQL />],
+        jobTitle: 'Backend Developer',
+        role:
+            'Responsible for the full backend development, my role was to create an API to store ' +
+            'submitted user data and generate an avatar using Imagick.',
+        title: 'Cravendale Milk Poster for Saatchi & Saatchi',
+        style: { clipPath: 'polygon(0px 0px, 100% 0px, 100% 99%, 0px 90%)' },
+        url: 'http://www.cravendale.co.uk/milkposter/'
     },
     {
-        color: "green",
-        description: "Teachers As Tutors provide experienced educators as tutors in London. A specialist" +
-        " tutoring service offering tuition and exam preparation for 7+, 8+, 11+ and CE.",
+        color: 'green',
+        description:
+            'Teachers As Tutors provide experienced educators as tutors in London. A specialist' +
+            ' tutoring service offering tuition and exam preparation for 7+, 8+, 11+ and CE.',
         heroSrc: tutorsHero,
-        jobTitle: "Freelance Full Stack Developer",
-        role: "The site was built in return for charity sponsorship for the Great North Run 2015. As well as providing" +
-        " information for parents, it also gave the tutors with the ability to setup lessons using a calendar system" +
-        " and issue invoices.",
-        title: "Teachers As Tutors",
+        icons: [<PHPIcon />, <Laravel />, <MySQL />, <JavaScript />, <Gulp />],
+        jobTitle: 'Freelance Full Stack Developer',
+        role:
+            'The site was built in return for charity sponsorship for the Great North Run 2015. As well as providing' +
+            ' information for parents, it also gave the tutors with the ability to setup lessons using a calendar system' +
+            ' and issue invoices.',
+        title: 'Teachers As Tutors',
         style: {
             clipPath: 'polygon(0px 0px, 100% 0px, 100% 86%, 0px 100%)'
         },
-        url: "https://www.teachersastutors.org/"
+        url: 'https://www.teachersastutors.org/'
     }
-]
+];
 
 const Projects = () => (
     <section className="flex w-full flex-wrap">
-        {projectsData.map((p, k) => <Project key={k} {...p}/>)}
+        {projectsData.map((p, k) => <Project key={k} {...p} />)}
     </section>
 );
 
