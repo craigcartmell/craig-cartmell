826fb4c192ca03668c6efe9b959b2bbb8a78a0e2
Craig Cartmell <craigcartmell1@gmail.com>
2018-01-07 16:58:03 +0000

    Header component

--- src/App.js
+++ src/App.js
@@ -1,110 +1,14 @@
 import React, { Component } from 'react';
-import defaultConfig from 'tailwindcss/defaultConfig'
 import './App.css';
-import logo from './images/logo-white.png';
-import yunojuno from './images/yunojuno.svg';
 
+import Header from './components/Header';
 import Projects from './components/Projects';
-
-import Icon from 'react-icons-kit';
-import { linkedin } from 'react-icons-kit/icomoon';
-import { github } from 'react-icons-kit/icomoon';
-import { twitter } from 'react-icons-kit/icomoon';
-import { cloudDownload } from 'react-icons-kit/icomoon';
-import { mail4 } from 'react-icons-kit/icomoon';
 
 class App extends Component {
     render() {
         return (
             <div className="w-full h-full bg-blue-darkest text-white">
-                <header className="flex w-full h-auto p-4 y-4">
-                    <div className="flex-1">
-                        <img
-                            alt="Craig Cartmell - Full Stack Developer"
-                            className="flex-1 w-1/3 min-w-0"
-                            src={logo}
-                        />
-                    </div>
-                    <div className="flex-1">
-                        <div className="flex h-full float-right items-center">
-                           <span className="mr-6">
-                                <a
-                                  href="https://uk.yunojuno.com/p/craig-cartmell"
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                                  title="YunoJuno"
-                                >
-                                  <img alt="YunoJuno" className="w-6" src={yunojuno} />
-                                </a>
-                            </span>
-                            <span className="mr-6">
-                                <a
-                                    href="https://www.linkedin.com/in/craigcartmell1/"
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                    title="LinkedIn"
-                                >
-                                    {/*<span style={{color: "#0077B5"}}>*/}
-                                    <span style={{ color: defaultConfig().colors["white"] }}>
-                                        <Icon icon={linkedin} size={24} />
-                                    </span>
-                                </a>
-                            </span>
-                            <span className="mr-6">
-                                <a
-                                  href="https://uk.yunojuno.com/profile/p/craig-cartmell/cv"
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                                  title="CV"
-                                >
-                                    {/*<span style={{color: "#0077B5"}}>*/}
-                                  <span style={{ color: defaultConfig().colors["white"] }}>
-                                        <Icon icon={cloudDownload} size={24} />
-                                    </span>
-                                </a>
-                            </span>
-                            <span className="mr-6">
-                                <a
-                                  href="mailto:craigcartmell1@gmail.com"
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                                  title="Email"
-                                >
-                                    {/*<span style={{color: "#0077B5"}}>*/}
-                                  <span style={{ color: defaultConfig().colors["white"] }}>
-                                        <Icon icon={mail4} size={24} />
-                                    </span>
-                                </a>
-                            </span>
-                            <span className="mr-6">
-                                <a
-                                    href="https://github.com/craigcartmell"
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                    title="Github"
-                                >
-                                    {/*<span style={{color: "#181717"}}>*/}
-                                    <span style={{ color: defaultConfig().colors["white"] }}>
-                                        <Icon icon={github} size={24} />
-                                    </span>
-                                </a>
-                            </span>
-                            <span>
-                                <a
-                                    href="https://twitter.com/craigcartmell1"
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                    title="Twitter"
-                                >
-                                    {/*<span style={{color: '#1DA1F2'}}>*/}
-                                    <span style={{ color: defaultConfig().colors["white"] }}>
-                                        <Icon icon={twitter} size={24} />
-                                    </span>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                </header>
+                <Header />
 
                 <section className="flex w-full flex-wrap">
                     <div className="w-1/4 h-1 bg-pink" />
@@ -123,7 +27,7 @@ class App extends Component {
                       with over 15 years in the industry.
                     </p>
                     <p className="m-4">
-                      I've been lucky enough to work for some of the biggest
+                      I've been lucky enough to work at some of the biggest
                       companies in their respective industries including{' '}
                       <span className="text-pink font-bold">
                             Saatchi & Saatchi
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Project from "../Project"
 
-import hero from '../../images/projects/exxon-mobil-sales-supervisor-excellence.png';
-import sc from '../../images/projects/jcdecaux-smart-content-new.png';
-import crav from '../../images/projects/cravendale-milkposter.png';
-import tat from '../../images/projects/teachers-as-tutors.png';
-import theDoor from '../../images/projects/saatchi-the-door.png';
-import dashboard from '../../images/projects/jcdecaux-smart-content-dashboard.png';
+import exxonSalesHero from '../../images/projects/exxon-mobil-sales-supervisor-excellence.png';
+import scHero from '../../images/projects/jcdecaux-smart-content-new.png';
+import cravendaleMilkposterHero from '../../images/projects/cravendale-milkposter.png';
+import tutorsHero from '../../images/projects/teachers-as-tutors.png';
+import theDoorHero from '../../images/projects/saatchi-the-door.png';
+import scDashboardHero from '../../images/projects/jcdecaux-smart-content-dashboard.png';
 
-const projs = [
+const projectsData = [
     {
         color: "pink",
         description: "SmartContent is a single page content management platform for managing digital out of home ads.",
@@ -17,14 +17,14 @@ const projs = [
         "help rebuild the content management system from the ground up. I was responsible " +
         "for the initial front-end platform build including liaising with the UX designer, choosing the " +
         "tech stack and creating the initial boilerplate.",
-        heroSrc: sc,
+        heroSrc: scHero,
         style: {clipPath: "polygon(0px 0px, 100% 0px, 100% 100%, 0px 90%)"},
         title: "SmartContent for Dynamic, JCDecaux",
     },
     {
         color: "indigo",
-        description: "A single page, real-time dashboard showing job failures and campaign analytics.",
-        heroSrc: dashboard,
+        description: "A single page, real-time scDashboardHero showing job failures and campaign analytics.",
+        heroSrc: scDashboardHero,
         jobTitle: "Senior Full Stack Developer",
         role: "A pet project to display critical information about campaigns and system performance. I built it using Apollo 2 " +
         "(GraphQL) server for the backend and Apollo 2 client with React for the front-end.",
@@ -32,13 +32,13 @@ const projs = [
         style: {
             clipPath: 'polygon(0 0, 100% 0, 100% 76%, 0 86%)'
         },
-        url: "https://github.com/craigcartmell/dashboard"
+        url: "https://github.com/craigcartmell/scDashboardHero"
     },
     {
         color: "teal",
         description: "The tool helps employees measure their own performance as well as allowing managers and" +
         " directors to view the statistical data using charts and graphs.",
-        heroSrc: hero,
+        heroSrc: exxonSalesHero,
         jobTitle: "Freelance Front-End Developer",
         title: "ExxonMobil Sales Supervisor Excellence for KHWS",
         role: "As the sole frontend developer, my role was to create a new Self Assessment Platform for ExxonMobil" +
@@ -51,7 +51,7 @@ const projs = [
         color: "orange",
         description: "A single page app which allowed Saatchi & Saatchi to find the brightest new grads. " +
         "The winners were offered 1 year paid work placement.",
-        heroSrc: theDoor,
+        heroSrc: theDoorHero,
         jobTitle: "Backend Developer",
         role: "Responsible for the full backend development, my role was to create a RESTful API to validate and " +
         "store submitted user data. Submissions could be viewed via an admin interface which allowed searching " +
@@ -64,7 +64,7 @@ const projs = [
         color: "blue",
         description: "A single page app that allows users to create their own cartoon avatar with a " +
         "custom slogan, and upload it to social media.",
-        heroSrc: crav,
+        heroSrc: cravendaleMilkposterHero,
         jobTitle: "Backend Developer",
         role: "Responsible for the full backend development, my role was to create an API to store " +
         "submitted user data and generate an avatar using Imagick.",
@@ -76,7 +76,7 @@ const projs = [
         color: "green",
         description: "Teachers As Tutors provide experienced educators as tutors in London. A specialist" +
         " tutoring service offering tuition and exam preparation for 7+, 8+, 11+ and CE.",
-        heroSrc: tat,
+        heroSrc: tutorsHero,
         jobTitle: "Freelance Full Stack Developer",
         role: "The site was built in return for charity sponsorship for the Great North Run 2015. As well as providing" +
         " information for parents, it also gave the tutors with the ability to setup lessons using a calendar system" +
@@ -91,7 +91,7 @@ const projs = [
 
 const Projects = () => (
     <section className="flex w-full flex-wrap">
-        {projs.map(p => <Project {...p}/>)}
+        {projectsData.map((p, k) => <Project key={k} {...p}/>)}
     </section>
 );
 
